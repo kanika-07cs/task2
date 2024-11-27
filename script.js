163048a7b047eb839a44b73f5977e37cb5afbb6c
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-11-27 07:45:26 +0530

    changed

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var change=0;
 var insertedMoney=0;
+var paidAmt=0;
 const coinOne=1;
 const coinTwo=2;
 const coinFive=5;
+
+function total(){
+    var coinOnes=Number(document.getElementById('oneCoin').value);
+    var coinTwos=Number(document.getElementById('twoCoin').value);
+    var coinFives=Number(document.getElementById('fiveCoin').value);
+
+    if(coinOnes>0){
+        coinOnes*=coinOne;
+    }
+    if(coinTwos>0){
+        coinTwos*=coinTwo;
+    }
+    if(coinFives>0){
+        coinFives*=coinFive;
+    }
+    paidAmt=coinOnes+coinTwos+coinFives;
+    return paidAmt.toFixed(2);
+}
+console.log(total());
