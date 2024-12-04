a36c2a7da302519ad7683dcd2b440ae53effe287
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-12-04 23:29:03 +0530

    changed

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-var change=0;
-var insertedMoney=0;
-var paidAmt=0;
-const coinOne=1;
-const coinTwo=2;
-const coinFive=5;
+function calcTotal(){
+    let total=0;
+    const productSelected=document.getElementById('selectedProducts');
+    productSelected.innerHTML=' ';
+    const content=document.querySelectorAll('#items input[type="number"]');
 
-function total(){
-    var coinOnes=Number(document.getElementById('oneCoin').value);
-    var coinTwos=Number(document.getElementById('twoCoin').value);
-    var coinFives=Number(document.getElementById('fiveCoin').value);
+    content.forEach(input=>{
+        const name=input.dataset.name;
+        const image=input.dataset.image;
+        const qty=parseInt(input.value,10)||0;
+        const price=parseInt(input.dataset.price,10);
 
-    if(coinOnes>0){
-        coinOnes*=coinOne;
-    }
-    if(coinTwos>0){
-        coinTwos*=coinTwo;
-    }
-    if(coinFives>0){
-        coinFives*=coinFive;
-    }
-    paidAmt=coinOnes+coinTwos+coinFives;
-    return paidAmt.toFixed(2);
-}
-function tally(){
-    insertedMoney=total();
-    document.getElementById('amt').innerHTML=insertedMoney;
-}
-function cancelTally(){
-    insertedMoney=0;
-    document.getElementById('amt').innerHTML=insertedMoney;
-}
-function clearAmt(){
-    document.getElementById('oneCoin').value=0;
-    document.getElementById('twoCoin').value=0;
-    document.getElementById('fiveCoin').value=0;
-}
-function clearAll(){
-    clearAmt();
-    cancelTally();
+        if(qty>0){
+            total+=qty*price;
+            const addedProduct=document.createElement('newOne');
+            addedProduct.classList.add('product-added');
+            addedProduct.innerHTML=`<img src="${image}" width="50px">
+            <span>${name}</span>
+            <span>Quantity:${qty}  Cost:${price*qty}</span>
+            `;
+
+            productSelected.appendChild(addedProduct);
+        }
+    });
+    document.getElementById('totalCost').innerText=total;
 }
